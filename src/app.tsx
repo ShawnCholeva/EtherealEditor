445bc5df330588c5b3adb541db58415833679840
ShawnCholeva <shawncholeva@gmail.com>
2017-12-24 12:59:33 -0600

    Added support for less files and less transpilation in webpack.config

--- src/app.tsx
+++ src/app.tsx
@@ -2,24 +2,13 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import SideMenu from './containers/side-menu';
-
-const mainWindowContainerStyle = {
-    border: '1px solid white',
-    height: '98vh'
-};
-
-const sideMenuContainerStyle = {
-    display: 'inline-block',
-    width: '15%',
-    border: '1px solid red',
-    height: '100%'
-};
+require('./app.less');
 
 class App extends Component {
     render() {
         return (
-            <div style={mainWindowContainerStyle}>
-                <div style={sideMenuContainerStyle}>
+            <div className='main-window-container'>
+                <div className='side-menu-container'>
                     <SideMenu />
                 </div>
             </div>
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,20 @@ const commonConfig = {
       {
         test: /\.tsx?$/,
         loader: 'ts-loader'
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "less-loader" // compiles Less to CSS
+          }
+        ]
       }
     ]
   },
